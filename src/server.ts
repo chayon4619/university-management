c5859442285063fae08d9233c416aefcd165452b
mrc4619 <mrchayon5525@gmail.com>
2023-05-28 23:09:30 +0600

    [DONE] setup express,mongoose,parser,cors successfully

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index";
+
+async function dbConnect() {
+    try {
+        await mongoose.connect(config.database_url as string);
+        console.log("Database Connect Successfully");
+
+        app.listen(config.port, () => {
+            console.log(`Application is running on port ${config.port}`)
+        })
+    } catch (error) {
+        console.log('Failed to Connect ', error)
+    }
+}
+dbConnect()
