fde0910848d3e6b8bb975d1752b8a00edbd85412
mrc4619 <mrchayon5525@gmail.com>
2023-08-06 19:49:47 +0600

    test commit

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
+import { logger, errorLogger } from './shared/logger'
 
 async function dbConnect() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log('Database Connect Successfully')
+    logger.info('Database Connect Successfully')
 
     app.listen(config.port, () => {
-      console.log(`Application is running on port ${config.port}`)
+      logger.info(`Application is running on port ${config.port}`)
     })
   } catch (error) {
-    console.log('Failed to Connect ', error)
+    errorLogger.error('Failed to Connect ', error)
   }
 }
 dbConnect()
